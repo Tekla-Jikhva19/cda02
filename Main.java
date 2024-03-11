0a4a3e5da4c4c28d19e2df03185da9f2c071fe2c
Tekla-Jikhva19 <158082310+Tekla-Jikhva19@users.noreply.github.com>
2024-03-11 10:46:49 +0400

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,18 @@
+public class Main {
+
+    public static void main(String[] args) {
+        int a = 12;
+        int b = 1;
+        if (a > b) {
+
+
+            System.out.println("a is more than b");
+        } else {
+            System.out.println("a is not  more than b");
+        }
+        for (int i = 0; i < 100; i++) {
+            System.out.println(i);
+        }
+        System.out.println(a + b);
+    }
+}
